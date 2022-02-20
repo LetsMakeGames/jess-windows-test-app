7198e9b8234fff6829ca523c1b1add5c0ad3e3e7
Jessica Barnes <jakebarnes1989@gmail.com>
2022-02-20 03:28:07 -0500

    Testing app home abstraction

---
+++ app-home.js
@@ -0,0 +1,35 @@
+const app = require("./app.js")
+
+function homeOpened (event, client, logger) {
+    // Call views.publish with the built-in client
+    const result = await client.views.publish({
+        // Use the user ID associated with the event
+        user_id: event.user,
+        view: {
+          // Home tabs must be enabled in your app configuration page under "App Home"
+          "type": "home",
+          "blocks": [
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "*Welcome home, <@" + event.user + "> :house:*"
+              }
+            },
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>."
+              }
+            }
+          ]
+        }
+      });
+  
+      logger.info(result);
+}
+
+module.exports = {
+    homeOpened
+};
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { App } = require('@slack/bolt');
+const appHome = require('./app-home.js')
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
@@ -6,9 +7,22 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+app.event('app_home_opened', async ({ event, client, logger }) => {
+    try {
+        appHome.homeOpened(event, client, logger);
+    }
+    catch (error) {
+      logger.error(error);
+    }
+  });
+
 (async () => {
   // Start your app
   await app.start(Number(process.env.PORT || 3000));
 
   console.log('⚡️ Bolt app is running!');
 })();
+
+module.exports = {
+    app
+};
